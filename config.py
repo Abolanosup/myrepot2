ff152a5d959fc8bacf5a6edfdc748e2d9c661896
Abolanosup <104039035+Abolanosup@users.noreply.github.com>
2022-12-18 23:15:48 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN = '' #Aqui va el token del bot
-API_ID =  '17158127' #Tu api id de telegram
-API_HASH = 'd1a5974070430293b64fde5339591447' #Tu api id de telegram
+BOT_TOKEN = '5900217375:AAHJPCehya6lZUfuaXM831-okA0Jms0povg' #Aqui va el token del bot
+API_ID =  9902519 #Tu api id de telegram
+API_HASH = '9d8097d05bbc90a6ed2a7a81abcd4e8a' #Tu api id de telegram
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','reymichel2009').split(';')
+ACCES_USERS = os.environ.get('tl_admin_user','Abolanos3').split(';')
 
-static_proxy = 'socks5://152.206.119.59:9050'
+static_proxy = 'http://KJCKLHYGKEGDYDKDGKCIYEDHCFRHDELDDGLF'
 PROXY = ProxyCloud.parse(static_proxy)
-
 if PROXY:
   print(f'Proxy {PROXY.as_dict_proxy()}')
   
@@ -20,7 +19,7 @@ if PROXY:
 #ponerlos valor 0 siempre
 
 space = {}
-space['reymichel2009'] = 0
+space['Abolanos3'] = 0
 
 #
 #
